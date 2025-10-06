92fc92d755a1900fd616f5824652ef0f849f7a67
Tx270 <tomek.ulatowski@outlook.com>
2025-10-06 09:54:54 +0200

    06.10.2025

--- first-project/index.js
+++ first-project/index.js
@@ -1,14 +1,21 @@
-import { createServer } from 'node:http';
+import { createServer } from "node:http";
+import { URL } from "node:url";
+import { handlePath } from "./src/path_handlers.js";
 
 const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello world!');
+  const request_url = new URL(`http://${host}${req.url}`);
+
+  handlePath(request_url.pathname, req, res);
+
+  if (!res.writableEnded) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Site not found!\n");
+  }
 });
 
 const port = 8000;
 const host = "localhost";
 
-
 server.listen(port, host, () => {
-    console.log(`Server listening on http://${host}:${port}`);
+  console.log(`Server listening on http://${host}:${port}`);
 });
